fa98e17fbecf12b291857689c963e1a496212e5b
hazze <hhazze3@gmail.com>
2026-02-01 14:41:30 +0300

    feat(task repo interface): updated task repository interface for actual methods

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -1,3 +1,10 @@
 package domain
 
-type TaskRepository interface{}
+import "context"
+
+type TaskRepository interface {
+	Create(ctx context.Context, title string)
+	GetAllTasks(ctx context.Context)
+	UpdateTaskStatus(ctx context.Context, ID string, status bool)
+	DeleteTask(ctx context.Context, ID string)
+}
